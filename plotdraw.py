534f0606a5382f82df9d4b4f07fe6cfed3290ffe
eric <eric.emj.johnson@gmail.com>
2024-09-22 01:55:11 -0500

    WIP: working on making plotting scripts functional

--- plotdraw.py
+++ plotdraw.py
@@ -1,158 +1,158 @@
 #!/usr/bin/env python3
+import json
 import matplotlib.pyplot as pyplot
-import numpy, sys
+import numpy as np
+import argparse
+import sys
 
 wake_iterations = 3
 
-TYPE = None
-with open(sys.argv[1]) as f:
-    if '.history' in sys.argv[1]:
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Plot data from .history or .lod files.")
+    parser.add_argument("input_file", help="Path to the .history or .lod file.")
+    parser.add_argument("-x", "--x_axis", default=None, help="X-axis variable (default: AoA for history, yavg for lod).")
+    parser.add_argument("-y", "--y_axis", default=None, help="Y-axis variable (default: CL for history, Cl for lod).")
+    parser.add_argument("-a", "--aoa", type=float, help="Filter data by Angle of Attack (AoA).")
+    parser.add_argument("-m", "--mach", type=float, help="Filter data by Mach number.")
+    parser.add_argument("-b", "--beta", type=float, help="Filter data by beta angle.")
+    parser.add_argument("-w", "--wing", type=float, help="Filter data by wing (for lod files).")
+    parser.add_argument("-hl", "--headless", action="store_true", help="Run in headless mode (no plot display).")
+    
+    return parser.parse_args()
+
+def initialize_data(data_order):
+    return {variable: [] for variable in data_order}
+
+def read_file(args):
+    if '.history' in args.input_file:
+        data_order = ['Mach','AoA','Beta','CLo','CLi','CLtot','CDo','CDi','CDtot','CDt','CDtot_t',
+                      'CSo','CSi','CStot','L/D','E','CFxo','CFyo','CFzo','CFxi','CFyi','CFzi',
+                      'CFxtot','CFytot','CFztot','CMxo','CMyo','CMzo','CMxi','CMyi','CMzi',
+                      'CMxtot','CMytot','CMztot','T/QS']
+        X = args.x_axis or 'AoA'
+        Y = args.y_axis or 'CL'
         TYPE = 'history'
-        data_order = ['Mach', 'AoA', 'beta', 'CL', 'CDo', 'CDi', 'CDtot', 'CS',
-                      'L/D', 'E', 'CFx', 'CFz', 'CFy', 'CMx', 'CMy', 'CMz', 'T/QS']
-        data = {'Mach': [], 'AoA': [], 'beta': [], 'CL': [], 'CDo': [], 'CDi': [],
-                'CDtot': [], 'CS': [],'L/D': [], 'E': [], 'CFx': [], 'CFz': [],
-                'CFy': [], 'CMx': [], 'CMy': [], 'CMz': [], 'T/QS': []}
-        X = 'AoA'
-        Y = 'CL'
-    elif '.lod' in sys.argv[1]:
+    elif '.lod' in args.input_file:
+        data_order = ['wing', 'S', 'yavg', 'chord', 'v/vref', 'Cl', 'Cd', 'Cs', 'compname', 'Mach', 
+                      'AoA', 'beta', 'Cx', 'Cy', 'Cz', 'CMx', 'CMy', 'CMz']
+        X = args.x_axis or 'yavg'
+        Y = args.y_axis or 'Cl'
         TYPE = 'lod'
-        # data_order = ['wing', 'yavg', 'chord', 'Cl', 'Cd', 'Cs', 'compname',
-        #               'Mach', 'AoA', 'beta', 'CL', 'CD', 'CS',
-        #               'CFx', 'CFy', 'CFz', 'CMx', 'CMy', 'CMz']
-        data_order = ['wing', 'S', 'yavg', 'chord', 'v/vref',
-                'Cl', 'Cd', 'Cs', 'compname', 'Mach', 'AoA', 'beta',
-                'Cx', 'Cy', 'Cz', 'CMx', 'CMy', 'CMz']
-        data = {'wing': [], 'S': [], 'yavg': [], 'chord': [], 'v/vref': [],
-                'Cl': [], 'Cd': [], 'Cs': [], 'compname': [], 'Mach': [], 'AoA': [], 'beta': [],
-                'Cx': [], 'Cy': [], 'Cz': [], 'CMx': [], 'CMy': [], 'CMz': []}
-        X = 'yavg'
-        Y = 'Cl'
     else:
         print('Incorrect filetype')
-        exit()
-    input_txt = f.readlines()
+        sys.exit(1)
 
-db = []
-AOA = None
-MACH = None
-BETA = None
-WING = None
-HEADLESS = False
-for arg in sys.argv:
-    if arg.startswith('-x'):
-        X = arg[2:]
-    elif arg.startswith('-y'):
-        Y = arg[2:]
-    elif arg.startswith('-a'):
-        AOA = float(arg[2:])
-    elif arg.startswith('-m'):
-        MACH = float(arg[2:])
-    elif arg.startswith('-b'):
-        BETA = float(arg[2:])
-    elif arg.startswith('-w'):
-        WING = float(arg[2:])
-    elif arg == '-h':
-        HEADLESS = True
+    data = initialize_data(data_order)
+    with open(args.input_file) as f:
+        input_txt = f.readlines()
+    
+    return data, data_order, X, Y, TYPE, input_txt
 
+def process_data(data, data_order, input_txt, TYPE):
+    db = []
+    if TYPE == 'history':
+        for i in range(len(input_txt)):
+            if input_txt[i].startswith('Solver Case:'):
+                l = i + 2 + wake_iterations
+                t = input_txt[l].split()[1:]  # Remove the first entry from the array
+                # Check if the lengths match
+                if len(t) != len(data_order):
+                    print(f"Warning: Mismatch between data_order length ({len(data_order)}) and line length ({len(t)})")
+                    print(f"Line content: {t}")
+                    continue  # Skip this line if lengths don't match
 
-def getdata(line):
-    while '  ' in line:
-        line = line.replace('  ', ' ')
-    line = line.split(' ')
-    line2 = []
-    for l in line:
-        if l != '':
-            line2.append(l)
-    return line2
+                dataset = {data_order[n]: t[n] for n in range(len(t))}
+                db.append(dataset)
+    else:
+        collect_w = False
+        for line in input_txt:
+            if line == '\n':
+                collect_w = False
+            elif collect_w:
+                values = line.split()[1:]  # Remove the first entry from the array
+                if len(values) != len(data_order):
+                    print(f"Warning: Mismatch between data_order length ({len(data_order)}) and line length ({len(values)})")
+                    print(f"Line content: {values}")
+                    continue  # Skip this line if lengths don't match
+                
+                dataset = {data_order[n]: values[n] for n in range(len(values))}
+                db.append(dataset)
+            elif line.startswith('   Wing'):
+                collect_w = True
+    
+    # Populate the data dictionary
+    for dataset in db:
+        for key in dataset.keys():
+            data[key].append(float(dataset[key]))
 
-if TYPE == 'history':
-    for i in range(len(input_txt)):
-        if input_txt[i].startswith('Solver Case:'):
-            l = i + 2 + wake_iterations
-            t = input_txt[l].split(' ')
-            while '' in t:
-                t.remove('')
-            t.remove(t[0])
-            t[len(t) - 1] = t[len(t) - 1][:-1]
-            dataset = {}
-            for n in range(0, len(t)):
-                data_name = data_order[n]
-                dataset[data_name] = t[n]
-            db.append(dataset)
-else:
-    collect_w = False
-    collect_c = False
-    for l in input_txt:
-        if l == '\n':
-            collect_w = False
-            collect_c = False
-        elif collect_w:
-            dataset = {}
-            for n in range(len(getdata(l[:-2]))):
-                dataset[data_order[n]] = getdata(l)[n]
-            db.append(dataset)
-        elif l.startswith('   Wing'):
-            collect_w = True
-        elif l.startswith('Comp'):
-            collect_c = True
+def format_cases(data, args, TYPE):
+    cases = {'AoA': [args.aoa, None], 'Beta': [args.beta, None], 'Mach': [args.mach, None]}
+    if TYPE == 'lod':
+        cases['wing'] = [args.wing, None]
+    
+    for c in cases.keys():
+        values = set(data[c])
+        if cases[c][0] is not None:
+            values = {cases[c][0]}
+        cases[c][1] = values
+    
+    return cases
 
-for d in db:
-    for k in d.keys():
-        data[k].append(float(d[k]))
+def plotter(data, first_set, second_set, param1, param2, X, Y, HEADLESS):
+    for first_val in first_set:
+        for second_val in second_set:
+            x_values = []
+            y_values = []
 
-if TYPE == 'history':
-    cases = {'AoA': [AOA, None], 'beta': [BETA, None], 'Mach': [MACH, None]}
-elif TYPE == 'lod':
-    cases = {'AoA': [AOA, None], 'beta': [BETA, None], 'Mach': [MACH, None], 'wing': [WING, None]}
+            for idx in range(len(data[X])):
+                two_is_good = data.get(param2, [None])[idx] == second_val or second_set == ['']
+                if data[param1][idx] == first_val and two_is_good:
+                    x_values.append(data[X][idx])
+                    y_values.append(data[Y][idx])
 
-def dataformater(inp, name):
-    arr = set([a for a in data[name]])
-    if inp is not None:
-        arr = set([inp])
-    return arr
+            if x_values and y_values:
+                label = f"{param1}={first_val}" + (f" {param2}={second_val}" if len(second_set) > 1 else "")
+                pyplot.plot(x_values, y_values, label=label)
 
-for c in cases.keys():
-    cases[c][1] = dataformater(cases[c][0], c)
-
-def plotter(first, second, name1, name2):
-    for f in first:
-        for s in second:
-            xres = []
-            yres = []
-            for i in range(len(data[X])):
-                try:
-                    if data[name2][i] == s or second == ['']:
-                        two_is_good = True
-                    else:
-                        two_is_good = False
-                except KeyError:
-                    two_is_good = True
-                if data[name1][i] == f and two_is_good:
-                    yres.append(data[Y][i])
-                    xres.append(data[X][i])
-            if yres != []:
-                if second == [''] or len(second) == 1:
-                    pyplot.plot(xres, yres, label=name1 + '=' + str(f))
-                else:
-                    pyplot.plot(xres, yres, label=name1 + '=' + str(f)  + ' ' + name2 + '=' + str(s))
                 if HEADLESS:
-                    print(str(xres) + ', ' + str(yres))
-        if second is ['']:
-            break
+                    print(f"x_values: {x_values}, y_values: {y_values}")
 
-if X == 'AoA':
-    plotter(cases['Mach'][1], cases['beta'][1], 'Mach', 'beta')
-elif X == 'Mach':
-    plotter(cases['AoA'][1], cases['beta'][1], 'AoA', 'beta')
-elif X == 'beta':
-    plotter(cases['AoA'][1], cases['Mach'][1], 'AoA', 'Mach')
-elif TYPE == 'lod':
-    print('lod')
-    plotter(cases['wing'][1], [''], 'wing', '')
+    # Set the title, x-label, and y-label
+    pyplot.title(f"{Y} vs {X}")  # Adding a title based on X and Y
+    pyplot.xlabel(X)
+    pyplot.ylabel(Y)
 
-pyplot.xlabel(X)
-pyplot.ylabel(Y)
-pyplot.legend()
-if not HEADLESS:
-    pyplot.show()
+    # Adjust the legend to be outside the plot on the right
+    pyplot.legend(loc='upper right', fontsize='small')
+
+    # Adjust layout to make space for the legend
+    pyplot.tight_layout(rect=[0, 0, 0.85, 1])
+    
+    if not HEADLESS:
+        pyplot.show()
+
+
+def main():
+    args = parse_arguments()
+    data, data_order, X, Y, TYPE, input_txt = read_file(args)
+    process_data(data, data_order, input_txt, TYPE)
+    cases = format_cases(data, args, TYPE)
+    
+    HEADLESS = args.headless
+    if X == 'AoA':
+        plotter(data, cases['Mach'][1], cases['Beta'][1], 'Mach', 'Beta', X, Y, HEADLESS)
+    elif X == 'Mach':
+        plotter(data, cases['AoA'][1], cases['Beta'][1], 'AoA', 'Beta', X, Y, HEADLESS)
+    elif X == 'Beta':
+        plotter(data, cases['AoA'][1], cases['Mach'][1], 'AoA', 'Mach', X, Y, HEADLESS)
+    elif TYPE == 'lod':
+        plotter(data, cases['wing'][1], [''], 'wing', '', X, Y, HEADLESS)
+    
+    pyplot.xlabel(X)
+    pyplot.ylabel(Y)
+    pyplot.legend()
+    if not HEADLESS:
+        pyplot.show()
+
+if __name__ == "__main__":
+    main()
--- plotlod.py
+++ plotlod.py
@@ -1,153 +1,126 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as pyplot
-import sys
 import json
+import argparse
 
 wake_iterations = 3
 
-TYPE = None
-with open(sys.argv[1]) as f:
-    if '.lod' in sys.argv[1]:
-        TYPE = 'lod'
-        data_order = ['wing', 'S', 'yavg', 'chord', 'v/vref', 'Cl', 'Cd', 'Cs',
-                      'Cx', 'Cy', 'Cz', 'CMx', 'CMy', 'CMz']
-        data_order_2 = ['comp', 'compname', 'Mach', 'AoA', 'beta', 'CL',
-                        'CDi', 'CS', 'CFx', 'CFy', 'CFz', 'Cmx', 'Cmy', 'Cmz']
-        data = {'wing': [], 'S': [], 'yavg': [], 'chord': [], 'v/vref': [],
-                'Cl': [], 'Cd': [], 'Cs': [], 'compname': [], 'Mach': [], 'AoA': [], 'beta': [],
-                'Cx': [], 'Cy': [], 'Cz': [], 'CMx': [], 'CMy': [], 'CMz': []}
-        data_comp = {'comp': {}, 'compname': {}, 'Mach': {}, 'AoA': {},
-                     'beta': {}, 'CL': {}, 'CDi': {}, 'CS': {}, 'CFx': {},
-                     'CFy': {}, 'CFz': {}, 'Cmx': {}, 'Cmy': {}, 'Cmz': {}}
-        X = 'yavg'
-        Y = 'Cl'
-    else:
-        print('Incorrect filetype')
-        exit()
-    input_txt = f.readlines()
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Plot data from .lod files.")
+    parser.add_argument("input_file", help="Path to the .lod file.")
+    parser.add_argument("-x", "--x_axis", default="yavg", help="X-axis variable (default: yavg).")
+    parser.add_argument("-y", "--y_axis", default="Cl", help="Y-axis variable (default: Cl).")
+    parser.add_argument("-a", "--aoa", default=None, help="Comma-separated list of AoA values to filter.")
+    parser.add_argument("-m", "--mach", default=None, help="Comma-separated list of Mach numbers to filter.")
+    parser.add_argument("-b", "--beta", default=None, help="Comma-separated list of beta values to filter.")
+    parser.add_argument("-w", "--wing", default=None, help="Comma-separated list of wing numbers to filter.")
+    
+    return parser.parse_args()
 
-db = []
-db_comp = []
-AOA = [None]
-MACH = [None]
-BETA = [None]
-WING = [None]
-for arg in sys.argv:
-    if arg.startswith('-x'):
-        X = arg[2:]
-    elif arg.startswith('-y'):
-        Y = arg[2:]
-    elif arg.startswith('-a'):
-        AOA = [float(a) for a in arg[2:].split(',')]
-    elif arg.startswith('-m'):
-        MACH = [float(a) for a in arg[2:].split(',')]
-    elif arg.startswith('-b'):
-        BETA = [float(a) for a in arg[2:].split(',')]
-    elif arg.startswith('-w'):
-        WING = [int(a) for a in arg[2:].split(',')]
-
+def initialize_data():
+    data_order = ['wing', 'S', 'yavg', 'chord', 'v/vref', 'Cl', 'Cd', 'Cs',
+                  'Cx', 'Cy', 'Cz', 'CMx', 'CMy', 'CMz']
+    data_order_2 = ['comp', 'compname', 'Mach', 'AoA', 'beta', 'CL',
+                    'CDi', 'CS', 'CFx', 'CFy', 'CFz', 'Cmx', 'Cmy', 'Cmz']
+    
+    data = {key: [] for key in data_order}
+    data_comp = {key: {} for key in data_order_2}
+    
+    return data, data_comp, data_order, data_order_2
 
 def getdata(line):
-    while '  ' in line:
-        line = line.replace('  ', ' ')
-    line = line.split(' ')
-    line2 = []
-    for l in line:
-        if l != '':
-            line2.append(l)
-    final = []
-    for l in line2:
-        try:
-            final.append(float(l))
-        except ValueError:
-            if l != '':
-                final.append(l)
-    return line2
+    line = ' '.join(line.split())  # Removes redundant spaces
+    return [float(val) if val.replace('.', '', 1).isdigit() else val for val in line.split(' ') if val]
 
+def process_file(input_txt, data_order, data_order_2):
+    collect_w = False
+    collect_c = False
+    db = []
+    db_comp = []
+    db_temp = []
+    db_comp_temp = []
 
-collect_w = False
-collect_c = False
-db_temp = []
-db_comp_temp = []
-for l in input_txt:
-    if l == '\n':
-        collect_w = False
-        collect_c = False
-        if db_temp != []:
-            db.append(db_temp)
-        if db_comp_temp != []:
-            db_comp.append(db_comp_temp)
-        db_temp = []
-        db_comp_temp = []
-    elif collect_w:
-        dataset = {}
-        for n in range(len(getdata(l[:-2]))):
-            dataset[data_order[n]] = getdata(l)[n]
-        db_temp.append(dataset)
-    elif collect_c:
-        dataset = {}
-        for n in range(len(getdata(l[:-2]))):
-            dataset[data_order_2[n]] = getdata(l)[n]
-        db_comp_temp.append(dataset)
-    elif l.startswith('   Wing'):
-        collect_w = True
-    elif l.startswith('Comp'):
-        collect_c = True
+    for line in input_txt:
+        if line.strip() == '':
+            collect_w = False
+            collect_c = False
+            if db_temp:
+                db.append(db_temp)
+            if db_comp_temp:
+                db_comp.append(db_comp_temp)
+            db_temp = []
+            db_comp_temp = []
+        elif collect_w:
+            dataset = {data_order[n]: getdata(line)[n] for n in range(len(getdata(line)))}
+            db_temp.append(dataset)
+        elif collect_c:
+            dataset = {data_order_2[n]: getdata(line)[n] for n in range(len(getdata(line)))}
+            db_comp_temp.append(dataset)
+        elif line.startswith('   Wing'):
+            collect_w = True
+        elif line.startswith('Comp'):
+            collect_c = True
 
-with open('./debug.json', 'w+') as jf:
-    jf.write(json.dumps(db, sort_keys=True, indent=4))
+    return db, db_comp
 
-yres = dict()
-xres = dict()
-yall = []
-xall = []
-for i in range(len(db)):
-    aoa = float(db_comp[i][0]['AoA'])
-    beta = float(db_comp[i][0]['beta'])
-    mach = float(db_comp[i][0]['Mach'])
-    go = True
-    if aoa not in AOA and None not in AOA:
-        go = False
-    if beta not in BETA and None not in BETA:
-        go = False
-    if mach not in MACH and None not in MACH:
-        go = False
-    if go:
-        for d in db[i]:
-            if int(d['wing']) in WING or None in WING:
-                if d['wing'] not in yres:
+def filter_data(db, db_comp, AOA, MACH, BETA, WING, X, Y):
+    yres = {}
+    xres = {}
+    yall = []
+    xall = []
+
+    for i in range(len(db)):
+        aoa = float(db_comp[i][0]['AoA'])
+        beta = float(db_comp[i][0]['beta'])
+        mach = float(db_comp[i][0]['Mach'])
+        
+        if (aoa in AOA or None in AOA) and (beta in BETA or None in BETA) and (mach in MACH or None in MACH):
+            for d in db[i]:
+                if int(d['wing']) in WING or None in WING:
                     wing_index = d['wing']
-                    yres[wing_index] = list()
-                    xres[wing_index] = list()
-                yres[d['wing']].append(float(d[Y]))
-                xres[d['wing']].append(float(d[X]))
-                yall.append(float(d[Y]))
-                xall.append(float(d[X]))
-                txt = 'AoA=' + str(aoa).rstrip('0').rstrip('.')
-                txt += ' beta=' + str(beta).rstrip('0').rstrip('.')
-                txt += ' Mach=' + str(mach).rstrip('0').rstrip('.')
-        if yres != []:
-            for k in yres.keys():
-                pyplot.plot(xres[k], yres[k],
-                            label=(txt + ' wing=' +
-                                   str(db_comp[i][int(k) - 1]['compname'])))
+                    yres.setdefault(wing_index, []).append(float(d[Y]))
+                    xres.setdefault(wing_index, []).append(float(d[X]))
+                    yall.append(float(d[Y]))
+                    xall.append(float(d[X]))
+                    
+                    txt = f"AoA={aoa} beta={beta} Mach={mach} wing={wing_index}"
+                    pyplot.plot(xres[wing_index], yres[wing_index], label=txt)
+    
+    return xall, yall
 
-    yres = dict()
-    xres = dict()
+def main():
+    args = parse_arguments()
 
+    # Initialize data and data_order lists
+    data, data_comp, data_order, data_order_2 = initialize_data()
+    
+    # Parse user-provided filters
+    AOA = list(map(float, args.aoa.split(','))) if args.aoa else [None]
+    MACH = list(map(float, args.mach.split(','))) if args.mach else [None]
+    BETA = list(map(float, args.beta.split(','))) if args.beta else [None]
+    WING = list(map(int, args.wing.split(','))) if args.wing else [None]
+    
+    # Read the input file
+    with open(args.input_file, 'r') as f:
+        input_txt = f.readlines()
+    
+    # Process the file
+    db, db_comp = process_file(input_txt, data_order, data_order_2)
+    
+    # Save to debug.json for debugging purposes
+    with open('./debug.json', 'w+') as jf:
+        json.dump(db, jf, sort_keys=True, indent=4)
+    
+    # Plotting filtered data
+    xall, yall = filter_data(db, db_comp, AOA, MACH, BETA, WING, args.x_axis, args.y_axis)
+    
+    # Configure the plot
+    pyplot.xlabel(args.x_axis)
+    pyplot.ylabel(args.y_axis)
+    pyplot.title(f"{args.y_axis} vs {args.x_axis}")
+    pyplot.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize='small')
+    pyplot.tight_layout(rect=[0, 0, 0.85, 1])
+    pyplot.show()
 
-def nth(ls, n):
-    out = []
-    for i in range(len(ls)):
-        if i % n == 0:
-            out.append(ls[i])
-
-
-N = 100
-pyplot.xlabel(X)
-pyplot.ylabel(Y)
-# print(numpy.linspace(min(yall), max(yall) + 0.1, 10))
-# pyplot.xticks(nth(xall, N))
-# pyplot.yticks(nth(yall, N))
-pyplot.legend()
-pyplot.show()
+if __name__ == "__main__":
+    main()
